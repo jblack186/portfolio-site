a78b302be085fc64c7d19d2c24189d76c022814b
Jamison Blackwell <blackwellj1040@gmail.com>
2020-06-17 10:57:22 -0400

    form is connected to gmail

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $email_from = 'https://clever-cori-f0c030.netlify.app/';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name\n".
+                    "User Email: $email.\n".
+                        "User Message: $message.\n";
+
+
+
+    $to = "blackwellj1040@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+ 
+    $headers = "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body, $headers);
+
+    header("location: index.html");
+
+?>
